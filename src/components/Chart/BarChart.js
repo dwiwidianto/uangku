3ae16c460e3d0c31337349efa81256e5cf1d971d
dwiwidianto <dwiwidianto00@gmail.com>
2021-10-15 21:54:30 +0700

    bug fix chart

--- src/components/Chart/BarChart.js
+++ src/components/Chart/BarChart.js
@@ -1,89 +1,71 @@
-import React from 'react';
-import {
-     Tooltip,
-     XAxis,
-     YAxis,
-     Legend,
-     CartesianGrid,
-     LineChart,
-     Line,
-} from 'recharts';
-import { getExpenseQuery, getIncomeQuery } from '../../graphql/Query';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { Tooltip, XAxis, YAxis, Legend, CartesianGrid, LineChart, Line, DateTime } from "recharts";
+import { getExpenseQuery, getIncomeQuery } from "../../graphql/Query";
+import { useQuery } from "@apollo/client";
 
 const Chart = () => {
-     const { data: expense } = useQuery(getExpenseQuery);
-     const { data: income } = useQuery(getIncomeQuery);
+  const { data: expense } = useQuery(getExpenseQuery);
+  const { data: income } = useQuery(getIncomeQuery);
 
-     console.table(expense && expense.name);
+  console.table(expense && expense.name);
 
-     return (
-          <div style={{ textAlign: 'center' }}>
-               <h1 className="mt-5 mb-5">Chart Money Tracker</h1>
-               <div className="App">
-                    <div className="container">
-                         <div className="row">
-                              <div className="col-md-6">
-                                   <LineChart
-                                        width={700}
-                                        height={250}
-                                        data={expense && expense.expense}
-                                        margin={{
-                                             top: 5,
-                                             right: 30,
-                                             left: 20,
-                                             bottom: 5,
-                                        }}
-                                   >
-                                        <XAxis dataKey="cost" />
-                                        <YAxis />
-                                        <CartesianGrid strokeDasharray="3 3" />
-                                        <Tooltip />
-                                        <Legend
-                                             verticalAlign="top"
-                                             height={36}
-                                        />
-                                        <Line
-                                             name="Expense Cost"
-                                             type="monotone"
-                                             dataKey="cost"
-                                             stroke="#8884d8"
-                                        />
-                                   </LineChart>
-                              </div>
-                              <div className="col-md-6">
-                                   <LineChart
-                                        width={700}
-                                        height={250}
-                                        data={income && income.income}
-                                        margin={{
-                                             top: 5,
-                                             right: 30,
-                                             left: 20,
-                                             bottom: 5,
-                                        }}
-                                   >
-                                        <XAxis dataKey="cost" />
-                                        <YAxis />
-                                        <CartesianGrid strokeDasharray="3 3" />
-                                        <Tooltip />
-                                        <Legend
-                                             verticalAlign="top"
-                                             height={36}
-                                        />
-                                        <Line
-                                             name="Income Cost"
-                                             type="monotone"
-                                             dataKey="cost"
-                                             stroke="#8884d8"
-                                        />
-                                   </LineChart>
-                              </div>
-                         </div>
-                    </div>
-               </div>
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1 className="mt-5 mb-5">Chart Money Tracker</h1>
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <LineChart
+                width={700}
+                height={250}
+                data={expense && expense.expense}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <XAxis dataKey="date" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Legend verticalAlign="top" height={36} />
+                <Line name="Expense Cost" type="monotone" dataKey="cost" stroke="#8884d8" />
+              </LineChart>
+            </div>
           </div>
-     );
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="col-md-4">
+                <LineChart
+                  width={700}
+                  height={250}
+                  data={income && income.income}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                >
+                  <XAxis dataKey="date" />
+                  <YAxis />
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <Tooltip />
+                  <Legend verticalAlign="top" height={36} />
+                  <Line name="Income Cost" type="monotone" dataKey="cost" stroke="#8884d8" />
+                </LineChart>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chart;
